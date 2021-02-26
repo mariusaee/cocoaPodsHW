782964e62ca4269dbb4063160c8f22782c8f847e
mariusaee <mariusae@me.com>
2021-02-26 18:44:08 +0300

    Label added in view

--- cocoaPodsHW/ViewController.swift
+++ cocoaPodsHW/ViewController.swift
@@ -10,19 +10,36 @@ import Spring
 class ViewController: UIViewController {
     
     @IBOutlet var animationView: SpringView!
+    @IBOutlet var animationLabel: UILabel!
     @IBOutlet var runButton: SpringButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        runButton.setTitle("Button Title", for: .normal)
+        animationView.layer.cornerRadius = 10
+        runButton.layer.cornerRadius = 10
+        runButton.setTitle("Run \(animationView.animation)", for: .normal)
     }
 
     @IBAction func runButtonPressed(_ sender: SpringButton) {
         animationView.animation = "squeeze"
+        animationView.curve = "spring"
+        animationView.force = 1
+        animationView.duration = 1
+        animationView.delay = 1
+        
         animationView.animate()
-        runButton.setTitle("Button", for: .normal)
-
-//        runButton.titleLabel = "asdf"
+        
+        animationLabel.text =
+            """
+            Name: \(animationView.animation)
+            Curve: \(animationView.animation)
+            Force: \(animationView.animation)
+            Duration: \(animationView.animation)
+            Delay: \(animationView.animation)
+            """
+        
+        runButton.setTitle("Run \(animationView.animation)", for: .normal)
+        
     }
 }
 
